6fe828f0ad944176b1af9c18d367b02b19a2e51f
hamRS <doriham123@gmail.com>
2025-07-01 20:58:34 -0300

    refactor remove nft marketplace

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -1,11 +1,9 @@
 import { WalletProvider } from './components/WalletProviders';
-import { NFTMarketplace } from './components/NFTMarketplace';
 
 function App() {
   return (
     <WalletProvider>
       <div className="min-h-screen bg-gray-100">
-        <NFTMarketplace />
       </div>
     </WalletProvider>
   );
